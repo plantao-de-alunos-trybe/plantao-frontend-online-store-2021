3a5bc4c6f9e36a51e19f940a6cd4b4bfffcb4071
Paulo Simões <paulohbsimoes@gmail.com>
2021-02-14 14:44:49 -0300

    Requisito 3 concluído

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/cart" component={ Cart } />
         <Route path="/" component={ Home } />
       </Switch>
     </Router>
