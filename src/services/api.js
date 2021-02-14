7e7cdbeb6eea761e07d2b40d8b8bd6906040999b
Paulo Simões <paulohbsimoes@gmail.com>
2021-02-14 14:32:27 -0300

    Requisito 1 concluído

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const enpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(enpoint);
+  return response.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query = '') {
+  const enpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const response = await fetch(enpoint);
+  return response.json();
 }
